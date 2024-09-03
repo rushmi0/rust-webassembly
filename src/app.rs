199301f3358ed68afe92a491475a18cb9c1f0f09
rushmi0 <gcalku@gmail.com>
2024-09-03 08:56:53 +0700

    update template

--- src/app.rs
+++ src/app.rs
@@ -1,10 +1,12 @@
+#![allow(non_snake_case)]
+
 use dioxus::prelude::*;
 use crate::page::home_page::HomePage;
 
 #[component]
 pub fn App() -> Element {
     rsx! {
-        link { rel: "stylesheet", href: "./main.css" }
+        link { rel: "stylesheet", href: "main.css" }
         HomePage {}
     }
 }
