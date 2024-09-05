2a770991f4fb1bfb4481644ec4fa8aec36f369e5
rushmi0 <gcalku@gmail.com>
2024-09-05 09:06:51 +0700

    update

--- src/components/counter.rs
+++ src/components/counter.rs
@@ -7,7 +7,15 @@ pub fn Counter() -> Element {
     let mut _count = use_signal(|| 0);
     rsx! {
         h1 { class: "text-white", "High-Five counter: {_count}" }
-        button { class: "text-white", onclick: move |_| _count += 1, "Up high!" }
-        button { class: "text-white", onclick: move |_| _count -= 1, "Down low!" }
+        button {
+            class: "text-white",
+            onclick: move |_| _count += 1,
+            "Up high!"
+        }
+        button {
+            class: "text-white",
+            onclick: move |_| _count -= 1,
+            "Down low!"
+        }
     }
 }
